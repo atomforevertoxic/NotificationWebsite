ff467499b1107d032d072c0d0a457882cd365185
Shatalov Ivan <139710109+atomforevertoxic@users.noreply.github.com>
2025-04-16 21:13:42 +0300

    User service implemented

---
+++ Services/UserService.cs
@@ -0,0 +1,38 @@
+public class UserService
+{
+    private readonly WebDbContext _context = default!;
+
+    public UserService(WebDbContext context)
+    {
+        _context = context;
+    }
+
+    public IList<User> GetUsers()
+    {
+        if (_context.Users != null)
+        {
+            return _context.Users.ToList();
+        }
+        return new List<User>();
+    }
+
+
+    public string AddUser(User user)
+    {
+        if (_context.Users != null)
+        {
+            if (_context.Users.Any(u => u.Email == user.Email))
+            {
+                return "This email is already subscribed";
+            }
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return "Email was successfully signed";
+        }
+
+        return "Database access error";
+    }
+
+    // уведомить пользователей?
+
+}
