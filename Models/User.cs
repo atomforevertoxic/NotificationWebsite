8f69f8772409f888a54a169a054c5cb4c64337e4
Shatalov Ivan <139710109+atomforevertoxic@users.noreply.github.com>
2025-04-16 20:16:32 +0300

    User model implemented

---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+class User
+{
+
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(20)]
+    public string Name { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [Range(18, 100)]
+    public int Age { get; set; }
+
+
+    public DateTime Timestamp { get; set; }
+}
